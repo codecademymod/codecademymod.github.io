0811c515fe5fe34b71879615e44440ea108ee691
codecademymod <55814189+codecademymod@users.noreply.github.com>
2019-09-26 15:50:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function mod(coverPhoto, profilePhoto, name, group, loc, date, handle, description) {
+function mod(coverPhoto, profilePhoto, name, group, loc, date, handle, description, forums, facebook, mobilize, discord, slack) {
 	newDiv = '' +
 	'<div class="col-md-3 col-sm-4">' +
 		 '<div class="card-container">' +
@@ -12,7 +12,7 @@ function mod(coverPhoto, profilePhoto, name, group, loc, date, handle, descripti
 					'</div>' +
 					'<div class="content">' +
 						'<div class="main">' +
-							'<h3 class="name">' +name+ '</h3>' +
+							'<h3 class="name"> '+ name+ '</h3>' +
 							'<p class="profession">' +group+ '</p>' +
 							'<p class="text-center">' +'<i class="fas fa-map-marker-alt">' +'</i>' + loc + '</p>' +
 							'<p class="text-center">' +'<i class="far fa-calendar-alt">' +'</i>' + date + '</p>' +
@@ -28,20 +28,30 @@ function mod(coverPhoto, profilePhoto, name, group, loc, date, handle, descripti
 					'</div>' +
 					'<div class="content">' +
 						'<div class="main">' +
-							'<h4 class="text-center"> @ ' +handle+ '</h4>' +
+							'<h4 class="text-center"> <a href="https://www.codecademy.com/profiles/'+handle+'" >@' +handle+ '</a></h4>' +
 							'<p class="text-center">' +description+ '</p>' +
 
 						'</div>' +
 					'</div>' +
 					'<div class="footer">' +
-						'<div class="social-links text-center">' +
-							'<a href="https://www.codecademy.com/profiles/'+handle+'" class="google">' +'<i class="fas fa-laptop-code">' +'</i>' +'</a>' +
-						'</div>' +
-					'</div>' +
-				'</div>' + 
-			'</div>' + 
-		'</div>' + 
-	'</div>';
+						'<div class="social-links text-center">';
 	
+	if(forums)
+		newDiv += '<i class="fab fa-discourse"></i>';
+	
+	if(facebook)
+		newDiv += '<i class="fab fa-facebook"></i>';
+	
+	if(mobilize)
+		newDiv += '<i class="fas fa-code"></i>';
+	
+	if(discord)
+		newDiv += '<i class="fab fa-discord"></i>';
+	
+	if(slack)
+		newDiv += '<i class="fab fa-slack"></i>';
+	
+	newDiv += '</div>' + '</div>' + '</div>' + '</div>' + '</div>' + '</div>';
+				
 	$("#modBox").append(newDiv);
 }
