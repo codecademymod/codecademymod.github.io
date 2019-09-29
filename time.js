4c6851ddced59cfad01abaa73eead1ffc5747ae8
codecademymod <55814189+codecademymod@users.noreply.github.com>
2019-09-29 22:22:11 +0100

    Update time.js

--- time.js
+++ time.js
@@ -3,7 +3,7 @@ const clocks = document.getElementsByClassName("clock");
 function updateClocks() {
     for (let clock of clocks) {
         let timezone = clock.dataset.timezone;
-        let time = new Date().toLocaleTimeString("en-US", {
+        let time = new Date().toLocaleTimeString("en-GB", {
             hour: '2-digit',
             minute: '2-digit',
             second: '2-digit',
